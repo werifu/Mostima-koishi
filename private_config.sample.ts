c71be7be3f2b6a85f4b3ebb4361bbcb7b6ccdce0
werifu <wohu1363195380@outlook.com>
2020-12-17 22:40:12 +0800

    sample config comment:

--- private_config.sample.ts
+++ private_config.sample.ts
@@ -1,2 +1,8 @@
-export const groups:number[] = [12345]
-export const owner:number = 12345
+// 主动上报的群号
+export const groups:number[] = [123456,234567]
+// 主人的qq（用于调试
+export const owner:number = 123456
+// 放新饼图绝对地址的前缀（本地文件需要file:///前缀
+export const picture_path_prefix:string = 'file:///D://xx/pictures/'
+// 放涩图绝对地址的前缀
+export const illustration_path_prefix:string = 'file:///D://xx/illustrations/'
