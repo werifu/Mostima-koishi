aa3839239377fc2db0463fb3f6f4d24f1d620704
werifu <wohu1363195380@outlook.com>
2020-12-29 00:31:54 +0800

    add new plugins

--- plugins/poke.ts
+++ plugins/poke.ts
@@ -1,9 +1,9 @@
 import {Context} from 'koishi-core';
 
 
-export const name: string = 'repeat';
+export const name: string = 'poke';
 export function apply(ctx: Context) {
     ctx.middleware(async (meta, next) => {
-
+        return next();
     })
 }
