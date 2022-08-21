bc028c8dfcf8b56205420fd143aece287b7e7107
werifu <wohu1363195380@outlook.com>
2022-08-21 14:21:01 +0800

    feat(repeat): random delay repeat

--- plugins/repeat.ts
+++ plugins/repeat.ts
@@ -1,25 +1,31 @@
-import {Context} from 'koishi-core';
-import { Groups } from '../private_config'
+import { randomInt } from 'crypto';
+import { Context } from 'koishi-core';
+import { Groups } from '../private_config';
 
-let lastMsgMap = new Map(Groups.map(group => [group.toString(), '']));
+const lastMsgMap = new Map(Groups.map((group) => [group.toString(), '']));
 let send_flag = false;
-export const name: string = 'repeat';
+export const name = 'repeat';
 
 export function apply(ctx: Context) {
-    ctx.group().middleware(async (session, next) => {
-        // console.log(session.content);
-        if (!session.content || !session.channelId) return next();
-        const groupId = session.channelId;
-        // 有人发了与上一条相同的句子而且bot没复读过
-        if (session.content === lastMsgMap.get(groupId) && send_flag === false) {
-            send_flag = true;
-            return session.send(session.content);
-        }
-        // 与上一条不一样
-        if (session.content && session.content !== lastMsgMap.get(groupId)) {
-            send_flag = false;
-            lastMsgMap.set(groupId, session.content);
-        }
-        return next();
-    })
+  ctx.group().middleware(async (session, next) => {
+    // console.log(session.content);
+    if (!session.content || !session.channelId) return next();
+    const groupId = session.channelId;
+    // 有人发了与上一条相同的句子而且bot没复读过
+    if (session.content === lastMsgMap.get(groupId) && send_flag === false) {
+      send_flag = true;
+      const randDelay = randomInt(0, 3600) * 1000;
+      // 随机延迟
+      setTimeout(() => {
+        session.content && session.send(session.content);
+      }, randDelay);
+      return;
+    }
+    // 与上一条不一样
+    if (session.content && session.content !== lastMsgMap.get(groupId)) {
+      send_flag = false;
+      lastMsgMap.set(groupId, session.content);
+    }
+    return next();
+  });
 }
